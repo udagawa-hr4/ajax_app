16a9701c4ae14c2f6bfedf906e2b50c4fa40d67b
udagawa-hr4 <hiro619881022@gmil.com>
2021-01-09 17:26:37 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
 
 
+      
     end
   end
 end
